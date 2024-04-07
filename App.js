5fd283aa4860828b36ca24977239e53312942e31
Mayur Marwade <mayumarwade899@gmail.com>
2024-04-07 19:19:03 +0530

    epidode 01 code added

---
+++ App.js
@@ -0,0 +1,37 @@
+//Nested structure in HTML
+{/* <div id="parent">
+    <div id="child">
+        <h1>This is an H1 Tag</h1>
+        <h2>This is an H2 Tag</h2>
+    </div>
+    <div id="child">
+        <h1>This is an H1 Tag</h1>
+        <h2>This is an H2 Tag</h2>
+    </div>
+</div> */}
+
+//Nested structure in React
+const parent = 
+React.createElement("div", {id:"parent"}, 
+    [
+        React.createElement("div", {id:"child"}, [
+            React.createElement("h1", {}, "This is a H1 tag!!!"),
+            React.createElement("h2", {}, "This is a H2 tag!!!")
+        ]),
+        React.createElement("div", {id:"child"}, [
+            React.createElement("h1", {}, "This is a H1 tag!!!"),
+            React.createElement("h2", {}, "This is a H2 tag!!!")
+        ])
+    ]
+)
+
+//This is an object (React Element)
+// const heading = React.createElement(
+//     "h1", 
+//     {id: "heading", xyz: "abc"}, 
+//     "Hey there!, hello from react"
+// );
+
+console.log(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
