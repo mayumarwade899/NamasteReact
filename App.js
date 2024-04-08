0f72a11082f310737b5cf880a630fd2e7f0fa378
Mayur Marwade <mayumarwade899@gmail.com>
2024-04-09 00:47:07 +0530

    JSX code added

--- App.js
+++ App.js
@@ -1,40 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-//Nested structure in HTML
-{/* <div id="parent">
-    <div id="child">
-        <h1>This is an H1 Tag</h1>
-        <h2>This is an H2 Tag</h2>
-    </div>
-    <div id="child">
-        <h1>This is an H1 Tag</h1>
-        <h2>This is an H2 Tag</h2>
-    </div>
-</div> */}
+//Using React
+// const heading = React.createElement("h1", {id:"heading"}, "This is React!");
+// console.log(heading);
 
-//Nested structure in React
-const parent = 
-React.createElement("div", {id:"parent"}, 
-    [
-        React.createElement("div", {id:"child"}, [
-            React.createElement("h1", {}, "This is a H1 tag!!!"),
-            React.createElement("h2", {}, "This is a H2 tag!!!")
-        ]),
-        React.createElement("div", {id:"child"}, [
-            React.createElement("h1", {}, "This is a H1 tag!!!"),
-            React.createElement("h2", {}, "This is a H2 tag!!!")
-        ])
-    ]
-)
+//Using JSX
+const jsxHeading = (
+  <h1 id="jsxHeading" className="heading">
+    This is React 🚀
+  </h1>
+);
 
-//This is an object (React Element)
-// const heading = React.createElement(
-//     "h1", 
-//     {id: "heading", xyz: "abc"}, 
-//     "Hey there!, hello from react"
-// );
-
-console.log(parent)
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
